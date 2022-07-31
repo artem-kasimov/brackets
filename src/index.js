22973fa7cfdd5396672663d437dd9c3292146351
bshk102 <xartem.kx@gmail.com>
2022-07-31 13:21:54 +0500

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+
+  const pairs = {}
+  bracketsConfig.forEach(pair => (pairs[pair[1]] = pair[0]))
+
+  const openingBrackets = Object.values(pairs)
+  const closingBrackets = Object.keys(pairs)
+
+  for (let i = 0; i < str.length; i++) {
+    if (openingBrackets.includes(str[i]) && closingBrackets.includes(str[i])) {
+      if (pairs[str[i]] === stack[stack.length - 1]) {
+        stack.pop()
+        continue
+      }
+      stack.push(str[i])
+      continue
+    } else if (openingBrackets.includes(str[i])) {
+      stack.push(str[i])
+      continue
+    }
+
+    if (stack.length === 0) return false
+
+    if (pairs[str[i]] === stack[stack.length - 1]) {
+      stack.pop()
+      continue
+    }
+
+    stack.push(str[i])
+  }
+
+  return stack.length === 0
 }
